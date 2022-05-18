9bc507e8aac3a2f964648ce33119e4614bba8344
Cristian Segura <cristian.segura.lepe@gmail.com>
2022-05-18 15:46:13 -0400

    adding some code

--- cssshcu.py
+++ cssshcu.py
@@ -1,8 +1,39 @@
 #!/bin/python3
 
-print("cssshcu")
+import paramiko
+import csv
+import sys
+import getopt
+
+REMOTE_SERVER_OUT_SEPARATOR=""
+
+REMOTE_SERVER_CMDS_LIST=[
+    "pwd",
+    "hostname",
+    "uname -a"
+]
+
+
+def main():
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", )
+    except getopt.GetOptError:
+        print("csshcu.py -s <servers-list.csv> -o <output-file.lst>")
+        sys.exit(2)
+    for opt, arg in opts:
 
 
 
 
+if __name__ == "__name__" :
+    main(sys.argv)
+
+
+
+#--------------#
+# References   #
+#--------------#
+# - Get Options: https://www.tutorialspoint.com/python3/python_command_line_arguments.htm
+#
 
